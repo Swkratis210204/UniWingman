379df11d5142ebe5526fa8c4e47f65f38dc54e36
PPavlou <periklispavlou04@gmail.com>
2026-04-08 02:35:17 +0300

    Some progress in map

---
+++ app/src/main/java/com/example/uniwingman/ui/map/MapFragment.java
@@ -0,0 +1,52 @@
+package com.example.uniwingman.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.uniwingman.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Δοκιμαστική πινέζα στην Αθήνα
+        LatLng athens = new LatLng(37.9838, 23.7275);
+        mMap.addMarker(new MarkerOptions()
+                .position(athens)
+                .title("Σημείο Παραλαβής Εύδοξος - Αθήνα"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(athens, 15f));
+    }
+}
